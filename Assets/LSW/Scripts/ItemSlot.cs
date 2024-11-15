ac4a2cbe72b00003eb46999996a2c075caef7955
lsw7791 <lsw7791@naver.com>
2024-11-16 03:37:02 +0900

    아이콘 클릭시 이름 표기

--- Assets/LSW/Scripts/ItemSlot.cs
+++ Assets/LSW/Scripts/ItemSlot.cs
@@ -9,14 +9,10 @@ public class ItemSlot : MonoBehaviour
     private ItemSO currentItem;  // 현재 슬롯의 아이템
     public UIEquip uiEquip;
     public UIInventory uiInventory;
-    // 외부 텍스트 UI를 할당하기 위한 변수
-    public Text itemNameText;  // 아이템 이름을 표시할 텍스트
-    public Text itemDescriptionText;  // 아이템 설명을 표시할 텍스트
 
     void Awake()
     {
-        itemNameText = uiInventory.itemNameText;
-        itemDescriptionText = uiInventory.itemDescriptionText;
+        uiInventory = FindObjectOfType<UIInventory>();
 
         // Icon이라는 이름의 자식 Image 컴포넌트를 찾아서 할당
         slotImage = transform.Find("Icon").GetComponent<Image>();
@@ -27,7 +23,7 @@ public class ItemSlot : MonoBehaviour
         }
     }
 
-    // 이미지와 아이템 할당 메서드
+    // 아이템을 슬롯에 설정하는 메서드
     public void SetItem(ItemSO item)
     {
         currentItem = item;
@@ -45,30 +41,11 @@ public class ItemSlot : MonoBehaviour
     // 아이콘 클릭 시 호출될 메서드
     public void OnClickIcon()
     {
-        if (currentItem != null && itemNameText != null && itemDescriptionText != null)
+        if (currentItem != null && uiInventory != null)
         {
-            // 아이템의 이름과 설명을 텍스트에 표시
-            itemNameText.text = currentItem.itemName;  // ItemSO에 정의된 아이템 이름
-            itemDescriptionText.text = currentItem.description;  // ItemSO에 정의된 아이템 설명
-        }
-    }
-
-    
-    public void OnEquip()
-    {
-        if (currentItem != null)
-        {
-            uiEquip.EquipItem(currentItem);
-
-            currentItem = null;
-            if (slotImage != null)
-            {
-                slotImage.sprite = null;
-                slotImage.enabled = false;
-            }
-
-            itemNameText.text = "";
-            itemDescriptionText.text = "";
+            // 아이템의 이름과 설명을 UI에 표시
+            uiInventory.itemNameText.text = currentItem.itemName;
+            uiInventory.itemDescriptionText.text = currentItem.description;
         }
     }
 }
