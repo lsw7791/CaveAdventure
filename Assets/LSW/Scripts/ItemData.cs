9024eaaf2cd391ac113e1ab351ad62bb6a85603e
lsw7791 <lsw7791@naver.com>
2024-11-19 09:25:57 +0900

    remove item / scripts

--- Assets/LSW/Scripts/ItemData.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class ItemData
-{
-    public ItemSO itemSO;
-    public int quantity;
-}
--- Assets/LSW/Scripts/ItemGetting.cs
+++
@@ -1,6 +0,0 @@
-
-public enum ItemGetting
-{
-    Pickup,       
-    Purchase,     
-}
