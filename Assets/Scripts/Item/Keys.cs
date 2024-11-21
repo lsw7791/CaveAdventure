6a7f296c46b170f5c32f170cc8c7e0b53c7455f8
BaekSeungWoo617 <koke100@naver.com>
2024-11-21 20:03:59 +0900

    Update Keys.cs

--- Assets/Scripts/Item/Keys.cs
+++ Assets/Scripts/Item/Keys.cs
@@ -3,12 +3,8 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Key : MonoBehaviour
 {
-    [SerializeField] private GameObject[] objectsToActivate; // 활성화할 오브젝트 배열
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.SetActive(false);
-
         if (collision.CompareTag("Player")) // 플레이어와 충돌 확인
         {
             gameObject.SetActive(false);
